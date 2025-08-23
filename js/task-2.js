488fac7a2a80b7ef49fc0858780b893e49efbb39
HilmiSafak <hilmisafak1@gmail.com>
2025-08-24 02:21:06 +0300

    Complate.

---
+++ js/task-2.js
@@ -0,0 +1,82 @@
+"use strict";
+/*
+İki parametre alan bir getUsersWithFriend(users, friendName) ok fonksiyonu yazın:
+
+ilk parametre users - kullanıcı nesnelerinden oluşan bir dizi
+ikinci parametre friendName - aranacak arkadaşın adı.
+Fonksiyon, friendName adında bir arkadaşı olan users dizisindeki tüm kullanıcıların bir dizisini döndürmelidir. 
+Her kullanıcının arkadaşları friends özelliğinde saklanır. 
+Böyle bir arkadaşı olan kullanıcı yoksa, fonksiyon boş bir dizi döndürmelidir.
+
+
+
+İpuçları:
+
+filter() metodu, belirli bir koşulu sağlayan öğelerle yeni bir dizi oluşturmak için kullanılabilir.
+friends dizisinin friendName içerip içermediğini kontrol etmek için includes() yöntemini kullanın.
+
+
+Aşağıdaki kodu alın ve doğru çalışıp çalışmadığını kontrol etmek için fonksiyonunuzun tanımladıktan sonra yapıştırın. 
+Konsol, çalışmanın sonuçlarını gösterecektir.
+*/
+
+function getUsersWithFriend(users, friendName) {
+  return users.filter((user) => user.friends.includes(friendName));
+}
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"],
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"],
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"],
+  },
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
---
+++ js/task-4.js
@@ -0,0 +1,60 @@
+"use strict";
+/* 
+İki parametre alan bir getTotalBalanceByGender(users, gender) ok fonksiyonu yazın:
+
+ilk parametre users - kullanıcı nesnelerinden oluşan bir dizi,
+ikinci parametre gender - cinsiyeti saklayan bir dize.
+Fonksiyon bir metot çağrı zinciri kullanmalı ve cinsiyeti (özellik gender) gender parametresinin 
+değeriyle eşleşen kullanıcıların toplam bakiyesini (özellik balance) döndürmelidir.
+
+Aşağıdaki kodu alın ve doğru çalışıp çalışmadığını kontrol etmek için fonksiyonunuzun tanımlanmasından sonra 
+yapıştırın. Çalışmasının sonuçları konsolda görüntülenecektir.
+*/
+
+const getTotalBalanceByGender = (users, gender) => {
+  return users.reduce((total, user) => {
+    return users.gender === gender ? total + user.balance : total;
+  }, 0);
+};
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
